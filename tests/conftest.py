63e15349ec2d386e10467a5f07e2efca4f97e27c
Roman <roman-boyko-91@mail.ru>
2025-09-09 19:48:59 +0500

    Commit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,5 @@
 import pytest
 
-from processing import list_dict
-
-
 @pytest.fixture
 def card_number():
     return "7020 7922 8960 6361"
@@ -14,10 +11,10 @@ def card_result():
 @pytest.fixture
 def filter_list_dict():
     return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}]
 
 @pytest.fixture
-def sort_by_date():
+def list_dict():
     return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
             {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
             {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
